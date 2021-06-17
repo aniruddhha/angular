4737b2fc82a0cb3838d2387cdf4604321fc94d85
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-06-17 12:23:32 +0530

    guard : authetication redirection

--- ang-app/src/app/auth.guard.ts
+++ ang-app/src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -10,12 +10,17 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private login: AuthService // dedicated service for perfroming authentication
+    private login: AuthService, // dedicated service for perfroming authentication
+    private router: Router
   ) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.login.isLoggedIn();
+
+    if (this.login.isLoggedIn()) {
+      return true;
+    }
+    return this.router.createUrlTree(['/404'])
   }
 }
